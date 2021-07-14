abe3a1b8c531b0fabfb04d79918080367ff46e45
facu2279 <facu2279@holbertonschool.com>
2021-07-14 15:15:57 +0000

    Fix

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -1,12 +1,12 @@
-	#include <stdio.h>
-	#include <dirent.h>
-	struct dirent *read;
-	/**
-	* main - main function
-	* Return: returns 0 when run correctly
-	*/
-	int main()
-	{
+#include <stdio.h>
+#include <dirent.h>
+struct dirent *read;
+/**
+* main - main function
+* Return: returns 0 when run correctly
+*/
+int main()
+{
 	DIR *dir;
 
 	dir = opendir(".");
