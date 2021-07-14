c416f141a3f11fa96a8b7207be1bd7e4ea53f5ef
facu2279 <facu2279@holbertonschool.com>
2021-07-14 21:03:48 +0000

    Fix

--- 0x00-ls/holberton.h
+++ 0x00-ls/holberton.h
@@ -1,9 +1,15 @@
+
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
-void print_dir(char *path);
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+
+char *bring_dir(char *path);
+int _strlen(char *s);
 int dir_check(char *dir_name);
-void print_dir_vertical(char *path);
 #endif
