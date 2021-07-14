6f211d40f1f82322cf31de76131138986491213b
facu2279 <facu2279@holbertonschool.com>
2021-07-14 19:17:28 +0000

    Fix

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -9,22 +9,44 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	char opcion = 'x';
 
 	if (argc == 1)
 		print_dir(".");
+	else if (argc == 2 && argv[1][1] == '1' && argv[1][0] == '-')
+		print_dir_vertical(".");
 	else
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 0; i < argc; i++)
 		{
-			printf("%s:\n", argv[i]);
-			print_dir(argv[i]);
-			if (i != argc - 1)
-				printf("\n");
+			if (argv[i][0] == '-')
+			{
+				opcion = argv[i][1];
+			}
+		}
+		if (opcion == 'x')
+		{
+			for (i = 1; i < argc; i++)
+			{
+				printf("%s:\n", argv[i]);
+				print_dir(argv[i]);
+				if (i != argc - 1)
+					printf("\n");
+			}
+		}
+		else if (opcion == '1')
+		{
+			for (i = 2; i < argc; i++)
+			{
+				printf("%s:\n", argv[i]);
+				print_dir_vertical(argv[i]);
+				if (i != argc - 1)
+					printf("\n");
+			}
 		}
 	}
 	return (0);
 }
-
 
 /**
 * print_dir - print_dir
@@ -40,6 +62,23 @@ void print_dir(char *path)
 		if (read->d_name[0] != '.')
 			printf("%s ", read->d_name);
 	printf("\n");
+	closedir(dir);
+
+}
+
+/**
+* print_dir_vertical - print_dir
+* @path: aaa
+*/
+void print_dir_vertical(char *path)
+{
+	struct dirent *read;
+	DIR *dir;
+
+	dir = opendir(path);
+	while ((read = readdir(dir)) != NULL)
+		if (read->d_name[0] != '.')
+			printf("%s\n", read->d_name);
 	closedir(dir);
 
 }
