b7207ba5156a5cd938cefa231548b49db75991c9
facu2279 <facu2279@holbertonschool.com>
2021-07-14 15:35:27 +0000

    task 1

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <dirent.h>
+
+void print_dir(char *path);
+
 /**
 * main - main function
 * Return: returns 0 when run correctly
 */
-int main(void)
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+		print_dir(".");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			printf("%s:\n", argv[i]);
+			print_dir(argv[i]);
+		}
+	}
+	return (0);
+}
+
+void print_dir(char *path)
 {
 	struct dirent *read;
 	DIR *dir;
 
-	dir = opendir(".");
+	dir = opendir(path);
 
 	while ((read = readdir(dir)) != NULL)
 		if (read->d_name[0] != '.')
 			printf("%s ", read->d_name);
 	printf("\n");
 	closedir(dir);
-
-	return (0);
 }
