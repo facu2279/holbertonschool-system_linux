de3cb04c84d5c27f07a100fa3c0ff036c1b50d3b
facu2279 <facu2279@holbertonschool.com>
2021-07-14 15:15:10 +0000

    Fix

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <dirent.h>
-struct dirent *read;
-/**
- * main - main function
- * Return: returns 0 when run correctly
- */
-int main()
-{
+	#include <stdio.h>
+	#include <dirent.h>
+	struct dirent *read;
+	/**
+	* main - main function
+	* Return: returns 0 when run correctly
+	*/
+	int main()
+	{
 	DIR *dir;
 
 	dir = opendir(".");
 
 	while ((read = readdir(dir)) != NULL)
-    	if (read->d_name[0] != '.')
+		if (read->d_name[0] != '.')
 			printf("%s ", read->d_name);
 	printf("\n");
 	closedir(dir);
