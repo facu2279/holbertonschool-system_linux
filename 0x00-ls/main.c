d04b5001748db09b6135a25c106d4927f78ff28b
facu2279 <facu2279@holbertonschool.com>
2021-07-14 15:49:02 +0000

    Fix

--- 0x00-ls/main.c
+++ 0x00-ls/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <dirent.h>
-
+#include <stdlib.h>
 void print_dir(char *path);
-
+int dir_check(char *dir_name);
 /**
 * main - main function
 * Return: returns 0 when run correctly
@@ -17,8 +17,16 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			printf("%s:\n", argv[i]);
-			print_dir(argv[i]);
+			if (dir_check(argv[i]) == 0)
+			{
+				printf("./hls: cannot access %s: No such file or directory\n", argv[i]);
+				exit(2);
+			}
+			else
+			{
+				printf("%s:\n", argv[i]);
+				print_dir(argv[i]);
+			}
 		}
 	}
 	return (0);
@@ -30,10 +38,29 @@ void print_dir(char *path)
 	DIR *dir;
 
 	dir = opendir(path);
-
 	while ((read = readdir(dir)) != NULL)
 		if (read->d_name[0] != '.')
 			printf("%s ", read->d_name);
 	printf("\n");
 	closedir(dir);
+
+}
+
+/**
+* dir_check - function to check if directory exists.
+* @dir_name: command or file name.
+*
+* Return: 1 on succes, 0 on failure.
+*/
+int dir_check(char *dir_name)
+{
+	DIR *dir = NULL;
+
+	dir = opendir(dir_name);
+	if (dir)
+	{
+		closedir(dir);
+		return (1);
+	}
+	return (0);
 }
