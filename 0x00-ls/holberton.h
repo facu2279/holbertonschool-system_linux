a1874f1c38addb69858bbc047b7d78b262f352ed
facu2279 <facu2279@holbertonschool.com>
2021-07-15 23:48:37 +0000

    Fix

--- 0x00-ls/holberton.h
+++ 0x00-ls/holberton.h
@@ -20,9 +20,7 @@ char **dir_finder(int argc, char *argv[]);
 void hfree(char **dirs, char *options);
 int dir_check2(char *dir_name);
 int else_main(int numero, char **dirs, int i, int bandera_numero,
-int salto, int dirs_len, char *array, int retoptions, char *options);
+int dirs_len, char *array, int retoptions, char *options);
 void handle_options(int retoptions, char *array);
 char *second_part_bring_dir(char *path, char *array2);
-void morejaja(int salto, int i, int file_count2, int dirs_len,
-char **dirs, int retoptions, char *array);
 #endif
--- 0x00-ls/options.c
+++ 0x00-ls/options.c
@@ -71,18 +71,3 @@ char *option_finder(int argc, char *argv[])
 
 	return (options);
 }
-
-/**
- * _strlen - fdfdf
- * @s: sksksks
- * Return: sassassass
- */
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
-}
