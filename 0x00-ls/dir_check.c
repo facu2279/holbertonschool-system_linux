4ee3fda8626e37ef36ef8b55977626c90fe498b2
facu2279 <facu2279@holbertonschool.com>
2021-07-15 21:44:04 +0000

    Fix

---
+++ 0x00-ls/dir_check.c
@@ -0,0 +1,72 @@
+#include "holberton.h"
+
+/**
+* dir_check - function to check if directory exists.
+* @dir_name: command or file name.
+* @functionn: command or file name.
+*
+* Return: 1 on succes, 0 on failure.
+*/
+int dir_check(char *dir_name, char *functionn)
+{
+	DIR *dir = NULL;
+
+	dir = opendir(dir_name);
+
+	if (dir == NULL)
+	{
+		if (errno == 2)
+		{
+			fprintf(stderr, "%s: cannot access %s: No such file or directory\n"
+			, functionn, dir_name);
+			closedir(dir);
+			return (2);
+		}
+		else if (errno == 13)
+		{
+			fprintf(stderr, "%s: cannot open directory %s: Permission denied\n"
+			, functionn, dir_name);
+			closedir(dir);
+			return (2);
+		}
+		else
+		{
+			printf("%s\n", dir_name);
+		}
+	}
+	closedir(dir);
+	return (0);
+}
+
+/**
+* dir_check2 - function to check if directory exists.
+* @dir_name: command or file name.
+*
+* Return: 1 on succes, 0 on failure.
+*/
+int dir_check2(char *dir_name)
+{
+	DIR *dir = NULL;
+
+	dir = opendir(dir_name);
+
+	if (dir == NULL)
+	{
+		if (errno == 2)
+		{
+			closedir(dir);
+			return (2);
+		}
+		else if (errno == 13)
+		{
+			closedir(dir);
+			return (2);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	closedir(dir);
+	return (0);
+}
--- 0x00-ls/holberton.h
+++ 0x00-ls/holberton.h
@@ -13,10 +13,14 @@ char *bring_dir_a(char *path);
 char *bring_dir_A(char *path);
 char *option_finder(int argc, char *argv[]);
 int _strlen(char *s);
-int dir_check(char *dir_name);
+int dir_check(char *dir_name, char *functionn);
 void print_uno(char *array);
 int chequear_opciones(char *options);
 char **dir_finder(int argc, char *argv[]);
 void hfree(char **dirs, char *options);
-
+int dir_check2(char *dir_name);
+int else_main(int numero, char **dirs, int i, int bandera_numero,
+int salto, int dirs_len, char *array, int retoptions, char *options);
+void handle_options(int retoptions, char *array);
+char *second_part_bring_dir(char *path, char *array2);
 #endif
