a5bc9157ecd872319296c641fa549e5e8e93f9d9
Facundo Diaz <70913784+facu2279@users.noreply.github.com>
2021-08-26 16:51:58 -0300

    test

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -1,22 +1,50 @@
 #!/usr/bin/python3
-""" Made by Facundo Diaz for Holberton School 2021"""
+"""
+"""
+
+
 import sys
 
+
+def represents_int(pid):
+    """
+    Args:
+        pid: number to be used as pid
+    Returns: True if can be converted to int, False otherwise
+    """
+    try:
+        int(pid)
+        return True
+    except ValueError:
+        return False
+
+
 def isascii_encodable(string):
-    """ """
+    """
+    Args:
+        string: String to check if can be ASCII encoded
+    Returns: True if can be ASCII encoded, false otherwise
+    """
     try:
         string.encode("ASCII")
         return True
     except UnicodeEncodeError:
         return False
 
-def cambiar():
-    """ """
+
+def parse_inputs():
+    """
+    This function checks that every input is the one that program expected
+    Returns:
+        pid: int with the process id
+        search_bytes: ASCII encoded search bytes
+        replace_bytes: ASCII encoded replace bytes
+    """
     if (len(sys.argv) != 4):
         print("Usage: ./read_write_heap [pid]"
               " [search_string] [replace_string]")
         exit(1)
-    if not to_int(sys.argv[1]):
+    if not represents_int(sys.argv[1]):
         print("{} must be an integer".format(sys.argv[1]))
         exit(1)
     [(print("{} must be ascii".format(x)), exit(1)) for x in sys.argv[2:]
@@ -24,16 +52,14 @@ def cambiar():
     return (int(sys.argv[1]), sys.argv[2].encode("ASCII"),
             sys.argv[3].encode("ASCII"))
 
-def to_int(num):
-    """ """
-    try:
-        int(num)
-        return True
-    except ValueError:
-        return False
 
-def posiciones(pid):
-    """ """
+def get_heap_positions(pid):
+    """
+    Args:
+        pid: number of the desired process
+    Returns:
+        range of the heap positions or corresponding error otherwise
+    """
     try:
         with open("/proc/{}/maps".format(pid)) as map_file:
             for line in map_file:
@@ -45,8 +71,16 @@ def posiciones(pid):
         print("Error in maps: {}".format(err))
         exit(1)
 
-def escribir(pid, search_bytes, replace_bytes, heap_start, heap_end):
-    """ """
+
+def write_inheap(pid, search_bytes, replace_bytes, heap_start, heap_end):
+    """
+    Args:
+        pid: number of the process
+        search_bytes: bytes to search for
+        replace_bytes: bytes to replace for
+        heap_start: initial heap number
+        heap_end: ending heap number
+    """
     try:
         with open("/proc/{}/mem".format(pid), "r+b") as mem_file:
             mem_file.seek(heap_start)
@@ -66,12 +100,18 @@ def escribir(pid, search_bytes, replace_bytes, heap_start, heap_end):
         print("Error in mem: {}".format(err))
         exit(1)
 
+
 def main():
-    pid, search_bytes, replace_bytes = cambiar()
+    """
+    Logic to get the user input, obtain the heap positions and overlaps
+    the search string with the replacement string
+    (Sudo permissions are needed)
+    """
+    pid, search_bytes, replace_bytes = parse_inputs()
     print("{} for {} in PID {}".format(search_bytes, replace_bytes, pid))
-    heap_start, heap_end = posiciones(pid)
+    heap_start, heap_end = get_heap_positions(pid)
     print("Heap bytes: 0x{:02x} - 0x{:02x}".format(heap_start, heap_end))
-    escribir(pid, search_bytes, replace_bytes, heap_start, heap_end)
+    write_inheap(pid, search_bytes, replace_bytes, heap_start, heap_end)
 
 if __name__ == "__main__":
     main()
