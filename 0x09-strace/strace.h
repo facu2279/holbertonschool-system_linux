4ece9616bb618267b97f0c6b7081f7975c575cd5
facu2279 <facu2279@holbertonschool.com>
2022-04-22 01:58:13 +0000

    Fix

---
+++ 0x09-strace/strace.h
@@ -0,0 +1,17 @@
+#ifndef STRACE_H
+#define STRACE_H
+
+#include "syscalls.h"
+
+#include <sys/ptrace.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/user.h>
+#include <string.h>
+
+#include <stdlib.h>
+#include <errno.h>
+
+#endif /* STRACE_H */
